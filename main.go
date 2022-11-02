b2d1a3e652f04c610f5ee7f799cf312b84fa4c66
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2022-11-02 22:40:40 +0100

    fix Synco

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/pterm/pterm"
 	"github.com/sandstorm/synco/cmd"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	//kubernetes.KubernetesInit()
+
+	// Fetch user interrupt - WORKAROUND: Somehow, this only works when in main.go - if we move it somewhere else, it will break.
+	// weird stuff :D I don't understand signals yet.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		pterm.Warning.Println("user interrupt")
+		os.Exit(0)
+	}()
+
 	cmd.Execute()
 }
